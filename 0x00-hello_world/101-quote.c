1cd26a3cc8fd5db84c88e5128817741c2409a1df
khawlaAlbasheer <khawla.elbasheer@gmail.com>
2023-02-16 23:06:53 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** main function
+ *
+ * return 1: success
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",61);
+	return (1);
+}
