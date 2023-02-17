7263c2ae24ae2238f5ae4d4c6667ac4bbabec9e6
khawlaAlbasheer <khawla.elbasheer@gmail.com>
2023-02-16 22:51:31 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* main function
+ * return 0: success
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+	printf("The size of int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("The size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("The size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("The size of char is: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("The size of float: %lu bte(s)\n", (unsigned long)sizeof(e));
+	return (0);
+	
+}
