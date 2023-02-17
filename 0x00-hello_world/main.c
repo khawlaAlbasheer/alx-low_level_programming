15093bc1862daf852871ebd95f1a00adfda68ca4
khawlaAlbasheer <khawla.elbasheer@gmail.com>
2023-02-16 21:26:16 -0800

    1-compiler

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	char f,m,l;
+	int age;
+	scanf ("%c %c %c %d",&f, &m, &l, &age); 
+	printf("my initials are %c%c%c and my age is %d.\n",f,m,l,age);
+	return(0);
+}
+
