6430fddfd74efaeaee55df0a3ffd77b4a57af973
khawlaAlbasheer <khawla.elbasheer@gmail.com>
2023-02-20 12:53:22 -0800

    a program that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /*
- * main - Entry point
- *
- * Return 0: success
+ * main - Prints all possible combinations of three different digits,
+ * in ascending order, separated by a comma followed by a space.
+ * Return Always 0: success
  */
 
 int main(void)
@@ -13,15 +12,16 @@ int main(void)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
+
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
-				if ( digit1 == 7 && digit2 == 8 && digit3 == 9)
-				{
-				       continue;
-				}
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+
 				putchar(',');
 				putchar(' ');
 			}
