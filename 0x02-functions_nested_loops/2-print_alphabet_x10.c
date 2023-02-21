9bda20607912a84ec1a8db612f36b34ed22acd23
khawlaAlbasheer <khawla.elbasheer@gmail.com>
2023-02-21 11:11:24 -0800

    a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print alphabet prints lowercase alphabet 10 times followed by a new line
+ * print_alphabet_x10 prints lowercase alphabet 10 times followed by a new line
  *
  * no return value
  */
