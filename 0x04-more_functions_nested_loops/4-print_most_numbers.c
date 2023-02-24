dd312502968f1f375b640fc4f1fb0797032de33d
khawlaAlbasheer <khawla.elbasheer@gmail.com>
2023-02-23 17:37:26 -0800

    a function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,9 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if ((i == 2) || (i == 4))
+		if ((i == 50) || (i == 52))
 		{
 			continue;
 		}
@@ -22,5 +22,5 @@ void print_most_numbers(void)
 			_putchar(i);
 		}
 	}
-	_putchar('\n');
+	_putchar(10);
 }
