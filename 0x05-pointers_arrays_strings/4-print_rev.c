2580ad820867a8a697aa9b4f9e8eb198649de6f9
khawlaAlbasheer <khawla.elbasheer@gmail.com>
2023-02-27 12:55:31 -0800

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string to evauate
+ * no return value
+ */
+
+void print_rev(char *s)
+{
+	int len;
+
+	len = _strlen(char *s);
+	for (int i = len; i = 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+}
