c8586e094d02c4d5006320132bc38b98d1864fc8
khawlaAlbasheer <khawla.elbasheer@gmail.com>
2023-03-08 14:57:30 -0800

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,3 +24,27 @@ int _sqrt_recursion(int n)
 		return (sqrt_manual(n, 1));
 	}
 }
+/**
+ * sqrt_manual - calculate manualy the sqrt of a number.
+ * @n: natural number
+ * @i: counter or number to be multiplied.
+ *
+ * Return: sqrt 0 if sqrt is not natural.
+ */
+int sqrt_manual(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i > n)
+	{
+
+		return (-1);
+	}
+	else
+	{
+
+		return (sqrt_manual(n, i + 1));
+	}
+}
