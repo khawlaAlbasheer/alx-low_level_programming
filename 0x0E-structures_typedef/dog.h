2237d8ca1128cd623d6979905edf184a8f4788d8
khawlaAlbasheer <khawla.elbasheer@gmail.com>
2023-03-21 12:40:33 +0200

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - define new types
+ * @name: string
+ * @age: int
+ * @owner: string
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
